3ca84f22edb0f3b5f3b38aa4e3fcfa6c1a0e528c
ysh <pzxcqwsx2@gmail.com>
2022-04-25 13:01:55 +0900

    Goals Setting

---
+++ Project1/Goals.cpp
@@ -0,0 +1,28 @@
+#define Baekjoon
+#define InterviewReady
+#define Portfolio
+#define NoGame
+#define Algorithm
+#define DesignPattern
+#define ComputerScience
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool isEmployment = false;
+
+	while (!isEmployment)
+	{
+		Baekjoon;
+		InterviewReady;
+		Portfolio;
+		NoGame;
+		Algorithm;
+		DesignPattern;
+		ComputerScience;
+	}
+
+	return 0;
+}
